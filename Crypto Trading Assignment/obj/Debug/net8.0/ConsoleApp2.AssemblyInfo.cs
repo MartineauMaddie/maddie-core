f5273fdae3ba41c17a24936665f3d08f6692a7a8
MartineauMadeline <madelinetcore@gmail.com>
2024-04-02 13:50:59 -0600

    Assignment 4 specs

--- Crypto Trading Assignment/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ Crypto Trading Assignment/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62ccac002c8edbc2e702898a2049f33d188fce36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10f922bf29b09ecd490072f32bffe90e613465e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
