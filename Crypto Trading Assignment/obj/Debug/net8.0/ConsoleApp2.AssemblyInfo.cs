f2c63090ced1b3923af82d1add7c2de1c74c2ac1
MartineauMadeline <madelinetcore@gmail.com>
2024-05-20 19:58:56 -0600

    Added Powershell profile

--- Crypto Trading Assignment/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ Crypto Trading Assignment/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b78e89318792106097b9bb83625319b23a0634c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfa42ff3df41dbbc01192c0fc12923369b0bc7fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
